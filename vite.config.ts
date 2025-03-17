37cdafe77d298915a086038a4f2d262704bcc32d
Beslan <beslandudaev8@gmail.com>
2025-03-17 10:54:49 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,13 @@ export default defineConfig({
   build: {
     outDir: "dist",
     rollupOptions: {
-      external: [],
+      external: ['react', 'react-dom', 'react-redux'], // добавьте react-redux в список внешних зависимостей
     },
   },
   server: {
-    port: 3000, // Локальный сервер на 3000 порту
+    port: 3000,
   },
   define: {
-    "process.env": {}, // Убирает ошибки с process.env в Vercel
+    "process.env": {}, // Убирает ошибки с process.env
   },
 });
