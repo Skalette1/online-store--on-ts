f2f60c749a7dcf17be67a12bb6d092576404303a
Beslan <beslandudaev8@gmail.com>
2025-03-17 22:18:00 +0300

    s

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { RouterProvider } from "react-router-dom";
+import { HashRouter, RouterProvider } from "react-router-dom";
 import { router } from "./router/Router";
 import { store } from "./store/store";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
+    <HashRouter>
     <RouterProvider router={router} />
+    </HashRouter>
   </Provider>,
 );
